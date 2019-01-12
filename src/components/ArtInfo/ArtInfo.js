f41cfee333cf9d9034da65adfc346127c284b17f
Cory Booth <Rodimus2005@hotmail.com>
2019-01-12 15:28:54 -0600

    general layout of info page

--- src/components/ArtInfo/ArtInfo.js
+++ src/components/ArtInfo/ArtInfo.js
@@ -21,29 +21,29 @@ import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 const styles = theme => ({
     card: {
-      maxWidth: 400,
+        maxWidth: 400,
     },
     media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
+        height: 0,
+        paddingTop: '56.25%', // 16:9
     },
     actions: {
-      display: 'flex',
+        display: 'flex',
     },
     expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
     },
     expandOpen: {
-      transform: 'rotate(180deg)',
+        transform: 'rotate(180deg)',
     },
     avatar: {
-      backgroundColor: red[500],
+        backgroundColor: red[500],
     },
-  });
+});
 
 const mapStateToProps = state => ({
     tileInfo: state.tileInfo,
@@ -59,85 +59,77 @@ class ArtInfo extends Component {
         }
     }
 
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
-  };
+    handleExpandClick = () => {
+        this.setState(state => ({ expanded: !state.expanded }));
+    };
 
     render() {
         const { classes } = this.props;
         return (
             <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title={this.props.tileInfo._source.title}
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              className={classes.media}
-              image={`https://1.api.artsmia.org/${this.props.tileInfo._id}.jpg`} alt={this.props.tileInfo._source.title}
-              title="Paella dish"
-            />
-            <CardContent>
-              <Typography component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.actions} disableActionSpacing>
-              <IconButton aria-label="Add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="Share">
-                <ShareIcon />
-              </IconButton>
-              <IconButton
-                className={classnames(classes.expand, {
-                  [classes.expandOpen]: this.state.expanded,
-                })}
-                onClick={this.handleExpandClick}
-                aria-expanded={this.state.expanded}
-                aria-label="Show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                  minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                  chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                  salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                  minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-                  to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-                  cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
+                <CardHeader
+                    //     avatar={
+                    //         <Avatar aria-label="Recipe" className={classes.avatar}>
+                    //             R
+                    // </Avatar>
+                    //     }
+                    //     action={
+                    //         <IconButton>
+                    //             <MoreVertIcon />
+                    //         </IconButton>
+                    //     }
+                    title={this.props.tileInfo._source.title}
+                    subheader={this.props.tileInfo._source.dated}
+                />
+                <CardMedia
+                    className={classes.media}
+                    image={`https://1.api.artsmia.org/${this.props.tileInfo._id}.jpg`} alt={this.props.tileInfo._source.title}
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography component="p">
+                        {this.props.tileInfo._source.artist}
+                    </Typography>
+                </CardContent>
+                <CardActions className={classes.actions} disableActionSpacing>
+                    <IconButton aria-label="Add to favorites" className={classes.icon}>
+                        <StarBorderIcon />
+                    </IconButton>
+                    {/* <IconButton aria-label="Add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="Share">
+                        <ShareIcon />
+                    </IconButton> */}
+                    <IconButton
+                        className={classnames(classes.expand, {
+                            [classes.expandOpen]: this.state.expanded,
+                        })}
+                        onClick={this.handleExpandClick}
+                        aria-expanded={this.state.expanded}
+                        aria-label="Show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        {/* <Typography paragraph>More Info:</Typography> */}
+                        <Typography paragraph>
+                            Object: {this.props.tileInfo._source.object_name || 'Unknown'}
+                        </Typography>
+                        <Typography paragraph>
+                            Country: {this.props.tileInfo._source.country || 'Unknown'}
+                        </Typography>
+                        <Typography paragraph>
+                            Department: {this.props.tileInfo._source.department || 'Unknown'}
+                        </Typography>
+                        <Typography>
+                            Description: {this.props.tileInfo._source.description || 'Not Available'}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
         );
     }
 }
