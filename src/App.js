1f8a5d50ffcc2085fd69f755fbfe0157d44aa2a9
Cory Booth <Rodimus2005@hotmail.com>
2019-01-12 09:24:29 -0600

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-         sup
-        </header>
-      </div>
-    );
-  }
-}
+import LandingPage from './components/LandingPage/LandingPage';
+
+
+const App = () => (
+  <div>
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+        <Route
+          path="/home"
+          component={LandingPage}
+        />  
+        <Route render={() => <h1>4O4</h1>} />
+
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/components/LandingPage/tileData.js
@@ -0,0 +1,36 @@
+import image from './LEGO.jpg';
+
+const tileData = [
+       {
+         img: image,
+         title: 'Image',
+         author: 'author',
+      },
+      {
+        img: image,
+        title: 'Image',
+        author: 'author',
+     },
+     {
+        img: image,
+        title: 'Image',
+        author: 'author',
+     },
+     {
+        img: image,
+        title: 'Image',
+        author: 'author',
+     },
+     {
+        img: image,
+        title: 'Image',
+        author: 'author',
+     },
+     {
+        img: image,
+        title: 'Image',
+        author: 'author',
+     },
+    ]
+
+export default tileData;
